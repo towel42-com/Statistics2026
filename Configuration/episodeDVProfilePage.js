a924e83f057a0b61ccbdfaecccd1c57751d45fb2
Scott Aron Bloom <scott@towel42.com>
2026-08-01 01:37:24 +0100

    Added Episode DV profile pages

---
+++ Configuration/episodeDVProfilePage.js
@@ -0,0 +1,42 @@
+﻿define([`baseView`, `emby-button`, `emby-select`],
+    function (BaseView) {
+        `use strict`;
+
+        const pluginId = "291d866f-baad-464a-aed6-a4a8b95a8fd7";
+
+        function View(view, params) {
+            BaseView.apply(this, arguments);
+            loadStats(view);
+        };
+
+        function loadStats(view) {
+            Dashboard.showLoadingMsg();
+
+            ApiClient.getPluginConfiguration(pluginId).then(function (config) {
+                for (var h = 0, len = config.EpisodeDVProfileItems.Count; h < len; h++) {
+                    var innerText = ``
+                    var currEpisodeGroup = config.EpisodeDVProfileItems.Movies[h];
+                    if (!config.showUnknownDVProfileCount && config.EpisodeDVProfileItems.IsUnknownDolbyProfile)
+                        continue;
+
+                    currEpisodeGroup.Movies.forEach((v) => {
+                        var imageUrl = ApiClient.getImageUrl(v.Id, { type: "Primary", quality: 90 });
+                        innerText += `<a is="emby-linkbutton" href="/item?id=` + v.Id + `&serverId=` + config.ServerId + `"><img src="` + imageUrl + `" height="200px" alt="` + v.Name + `" /></a>`;
+                    });
+
+                    var currHtml = `<h2 id = "` + currEpisodeGroup.Title + `Title">` + currEpisodeGroup.Title + `</h2><div id="` + currEpisodeGroup.Title + `">` + innerText + `</div>`;
+                    view.querySelector("#pagestart").innerHTML += currHtml;
+                }
+
+                Dashboard.hideLoadingMsg();
+            });
+        };
+
+        Object.assign(View.prototype, BaseView.prototype);
+
+        View.prototype.onResume = function (options) {
+            BaseView.prototype.onResume.apply(this, arguments);
+        };
+
+        return View;
+    });
---
+++ Configuration/episodeDVProfileTextPage.js
@@ -0,0 +1,48 @@
+﻿define([`baseView`, `emby-button`, `emby-select`],
+    function (BaseView) {
+        `use strict`;
+
+        const pluginId = "291d866f-baad-464a-aed6-a4a8b95a8fd7";
+
+        function View(view, params) {
+            BaseView.apply(this, arguments);
+            loadStats(view);
+        };
+
+        function loadStats(view) {
+            Dashboard.showLoadingMsg();
+
+            ApiClient.getPluginConfiguration(pluginId).then(function (config) {
+                for (var h = 0, len = config.EpisodeDVProfileItems.Count; h < len; h++) {
+                    var innerText = ``
+                    var currEpisodeGroup = config.EpisodeDVProfileItems.Movies[h];
+                    if (!config.showUnknownDVProfileCount && config.EpisodeDVProfileItems.IsUnknownDolbyProfile)
+                        continue;
+
+                    currEpisodeGroup.Movies.forEach((v) => {
+                        innerText += makeTable(v, config.ServerId);
+                    });
+
+                    var currHtml = `<h2 id = "` + currEpisodeGroup.Title + `Title">` + currEpisodeGroup.Title + `</h2><div><table id="` + currEpisodeGroup.Title + `">` + innerText + `</table></div>`;
+                    view.querySelector("#pagestart").innerHTML += currHtml;
+                }
+
+                Dashboard.hideLoadingMsg();
+            });
+        };
+
+        function makeTable(movie, serverId) {
+            var html = `<tr>`;
+            html += `<td><a is="emby-linkbutton" href="/item?id=` + movie.Id + `&serverId=` + serverId + `">` + movie.Name + `</a></td>`;
+            html += `<td>` + movie.Year + `</td>`;
+            return html + `</tr>`;
+        }
+
+        Object.assign(View.prototype, BaseView.prototype);
+
+        View.prototype.onResume = function (options) {
+            BaseView.prototype.onResume.apply(this, arguments);
+        };
+
+        return View;
+    });
