46c867c44b9963367c53eb2c1672a0a06501873c
Scott Aron Bloom <scott@towel42.com>
2026-09-18 21:40:51 +0100

    Better reporting when a user table needs data

--- Statistics2026/Data/StatisticsDB_DBState.cs
+++ Statistics2026/Data/StatisticsDB_DBState.cs
@@ -1,7 +1,10 @@
 ﻿using MediaBrowser.Model.Querying;
+using ServiceStack.Text;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 
 namespace Statistics2026.Data
 {
@@ -11,7 +14,7 @@ namespace Statistics2026.Data
         {
             CheckIsValid( ECheckType.eInit );
 
-            if( Plugin.Instance!.DBStateInitialized() )
+            if( !Plugin.Instance!.Configuration.resetDBState && Plugin.Instance!.DBStateInitialized() )
                 return;
 
             Plugin.Instance.ResetDBState();
@@ -39,6 +42,9 @@ namespace Statistics2026.Data
             );
 
             ComputeUserDataDBState();
+
+            Plugin.Instance!.Configuration.resetDBState = false;
+            Statistics2026.Plugin.Instance.UpdateConfiguration( Plugin.Instance!.Configuration );
         }
 
         private void ComputeUserDataDBState()
@@ -59,6 +65,7 @@ namespace Statistics2026.Data
             }
 
             var tableNames = allUserMediaTables();
+            var missing = new List<List<object>>();
             _dbHelper.ValidateTables( tableNames,
                 tableName =>
                 {
@@ -66,67 +73,10 @@ namespace Statistics2026.Data
                 },
                 tableName =>
                 {
-                    var sqlTicks = $"SELECT COUNT(*) FROM {tableName} WHERE TotalTicksPlayed IS NOT NULL AND TotalTicksPlayed != 0";
-                    var sqlPlayed = $"SELECT COUNT(*) FROM {tableName} WHERE PlayCount>0 AND IsPlayed";
-
-                    long? ticksPlayed = null;
-                    long systemPlayed = 0;
-
-                    List<SQLCmdDef> cmds =
-                    [
-                        new( sqlTicks ),
-                        new( sqlPlayed )
-                    ];
-
-                    _dbHelper.ExecuteCommands( cmds, statement =>
-                    {
-                        var row = statement.Current;
-                        var value = row.GetInt64( 0 );
-                        if( ticksPlayed == null )
-                            ticksPlayed = value;
-                        else
-                            systemPlayed = value;
-                        return true;
-                    }
-                    );
-
-                    ticksPlayed ??= 0;
-
-                    _embyInterfaces!._logger.Debug( $"Table: {tableName} - # w/TicksPlayed {ticksPlayed} - # w/PlayCount {systemPlayed}" );
-                    if( ticksPlayed! != systemPlayed )
-                    {
-                        _embyInterfaces._logger.Debug( "The following rows do not have TotalTicksPlayed data" );
-
-                        var sql =
-                            $"SELECT * FROM {tableName} WHERE TotalTicksPlayed IS NOT NULL AND TotalTicksPlayed != 0" +
-                            $" EXCEPT " +
-                            $"SELECT * FROM {tableName} WHERE PlayCount>0 AND IsPlayed"
-                            ;
-
-                        _dbHelper.ExecuteCommand( new SQLCmdDef( sql ), statement =>
-                        {
-                            var row = statement.Current;
-                            var col = 0;
-                            var userId = row.GetString( col++ );
-                            var itemId = row.GetString( col++ );
-                            var name = row.GetString( col++ );
-                            var isPlayed = row.GetBoolean( col++ );
-                            var playCount = row.GetInt64( col++ );
-                            var lastPlayed = row.GetString( col++ );
-                            var startTickPos = row.GetInt64( col++ );
-                            var endTickPos = row.GetInt64( col++ );
-                            var totalTicksPlayed = row.GetInt64( col++ );
-                            var isEpisode = row.GetBoolean( col++ );
-                            var numEpisodes = row.GetInt64( col++ );
-                            var isTVSpecial = row.GetBoolean( col++ );
-                            var seriesId = row.GetString( col++ );
-
-                            _embyInterfaces._logger.Debug( $"{name} - {isPlayed} - {playCount} - {totalTicksPlayed}" );
-                            return true;
-                        } );
-                    }
-
-                    return ticksPlayed != systemPlayed;
+                    var missingRows = CheckUserMediaTableHasData( tableName );
+                    if ( missingRows != null )
+                        missing.AddRange( missingRows );
+                    return missingRows != null;
                 },
                 ( createTableNeeded, initDataNeeded ) =>
                 {
@@ -134,6 +84,172 @@ namespace Statistics2026.Data
                     Plugin.Instance.SetDBState( EDBState.eUserDataInitialized, !initDataNeeded );
                 }
             );
+
+            if ( missing != null && missing.Count > 0 )
+                dumpTable( [ "User", "Item ID", "Name", "Runtime Ticks", "Is Played?", "Play Count", "Total Ticks Played", "Series Name", "Season#", "Episode#" ], missing, text => _embyInterfaces?._logger.Warn( text ) );
+        }
+
+        private List<List<object>>? CheckUserMediaTableHasData( string tableName )
+        {
+            var sqlTicks = $"SELECT COUNT(*) FROM {tableName} WHERE TotalTicksPlayed IS NOT NULL AND TotalTicksPlayed != 0";
+            var sqlPlayed = $"SELECT COUNT(*) FROM {tableName} WHERE PlayCount>0 AND IsPlayed";
+
+            long? ticksPlayed = null;
+            long systemPlayed = 0;
+
+            List<SQLCmdDef> cmds =
+            [
+                new( sqlTicks ),
+                new( sqlPlayed )
+            ];
+
+            _dbHelper.ExecuteCommands( cmds, statement =>
+            {
+                var row = statement.Current;
+                var value = row.GetInt64( 0 );
+                if( ticksPlayed == null )
+                    ticksPlayed = value;
+                else
+                    systemPlayed = value;
+                return true;
+            }
+            );
+
+            ticksPlayed ??= 0;
+
+            //_embyInterfaces!._logger.Debug( $"Table: {tableName} - # w/TicksPlayed {ticksPlayed} - # w/PlayCount {systemPlayed}" );
+            List<List<object>>? missing = null;
+            if( ticksPlayed != systemPlayed && ticksPlayed != 0 )
+            {
+                var user = getUserForTableName( tableName );
+                if( user == null )
+                {
+                    _embyInterfaces?._logger.Warn( $"Invalid user table name {tableName}" );
+                }
+                else
+                {
+                    _embyInterfaces?._logger.Warn( $"User: {user.Name} has an invalid UserMedia table" );
+                }
+
+                var fields = $"Users.UserName, {tableName}.ItemID, {tableName}.Name, Media.RunTimeTicks, {tableName}.IsPlayed, {tableName}.PlayCount, {tableName}.TotalTicksPlayed, Series.Name, Media.Season, Media.Episode";
+                var joinClause = $" LEFT JOIN Users ON Users.UserId={tableName}.UserId LEFT JOIN Media ON Media.ItemId = {tableName}.ItemId LEFT JOIN Series On Series.ItemId={tableName}.SeriesId";
+
+                var pos = sqlTicks.IndexOf( "WHERE" );
+                if( pos != -1 )
+                    sqlTicks = sqlTicks.Insert( pos, joinClause + " " );
+
+                pos = sqlPlayed.IndexOf( "WHERE" );
+                if( pos != -1 )
+                    sqlPlayed = sqlPlayed.Insert( pos, joinClause + " " );
+
+                var sqlAtoB = sqlTicks + " EXCEPT " + sqlPlayed;
+                sqlAtoB = sqlAtoB.Replace( "COUNT(*)", fields );
+
+                var sqlBtoA = sqlPlayed + " EXCEPT " + sqlTicks;
+                sqlBtoA = sqlBtoA.Replace( "COUNT(*)", fields );
+
+                cmds =
+                [
+                    new( sqlAtoB ),
+                    new( sqlBtoA )
+                ];
+
+                missing = [];
+                _dbHelper.ExecuteCommands( cmds, statement =>
+                {
+                    var row = statement.Current;
+                    var col = 0;
+
+                    var userName = row.GetString( col++ );
+                    var itemId = row.GetString( col++ );
+                    var itemName = row.GetString( col++ );
+                    var runtimeTicks = row.GetInt64( col++ );
+                    var isPlayed = row.GetBoolean( col++ );
+                    var playCount = row.GetInt64( col++ );
+                    var totalTicksPlayed = row.GetInt64( col++ );
+                    var seriesName = row.GetString( col++ );
+                    var season = row.GetInt64( col++ );
+                    var episode = row.GetInt64( col++ );
+
+                    missing.Add( [ userName, itemId, itemName, runtimeTicks, isPlayed, playCount, totalTicksPlayed, seriesName, season, episode ] );
+
+                    return true;
+                } );
+            }
+
+            return ( ticksPlayed == systemPlayed ) ? null : missing;
+        }
+
+        public static void dumpTable( List<string> header, List<List<object>> rows, Action<string> dumpFunc )
+        {
+            List<int> columnWidths = [];
+            foreach( var item in header )
+            {
+                columnWidths.Add( item.Length + 2 );
+            }
+
+            foreach( var currRow in rows )
+            {
+                if( currRow.Count() != header.Count() )
+                {
+                    throw new InvalidDataContractException( "Header column count is different than row column count" );
+                }
+                for( var ii = 0; ii < currRow.Count(); ++ii )
+                {
+                    var currItemString = currRow[ ii ]?.ToString() ?? string.Empty;
+
+                    columnWidths[ ii ] = Math.Max( currItemString.Length + 2, columnWidths[ ii ] );
+                }
+            }
+
+            dumpRow( columnWidths, null, dumpFunc );
+            dumpRow( columnWidths, header.Cast<object>().ToList(), dumpFunc );
+            dumpRow( columnWidths, null, dumpFunc );
+            foreach( var currRow in rows )
+            {
+                dumpRow( columnWidths, currRow.Cast<object>().ToList(), dumpFunc );
+            }
+            dumpRow( columnWidths, null, dumpFunc );
+        }
+
+        public static void dumpRow( List<int> columnWidths, List<object>? items, Action<string> dumpFunc )
+        {
+            var rowString = string.Empty;
+            if( items == null )
+            {
+                for( var ii = 0; ii < columnWidths.Count(); ++ii )
+                {
+                    if( ii == 0 )
+                    {
+                        rowString += "|";
+                    }
+                    rowString += new string( '-', columnWidths[ ii ] );
+                    rowString += "|";
+                }
+            }
+            else
+            {
+                if( columnWidths.Count() != items.Count() )
+                {
+                    throw new InvalidDataContractException( "Column width column count is different than row column count" );
+                }
+
+                for( var ii = 0; ii < items.Count(); ++ii )
+                {
+                    var currItem = items[ ii ]?.ToString() ?? string.Empty;
+                    var totalPadding = columnWidths[ ii ] - currItem.Length;
+                    var leftPadding = totalPadding / 2;
+
+                    currItem = new string( ' ', leftPadding ) + currItem + new string( ' ', totalPadding - leftPadding );
+                    if( ii == 0 )
+                    {
+                        rowString += "|";
+                    }
+                    rowString += currItem;
+                    rowString += "|";
+                }
+            }
+            dumpFunc( rowString );
         }
     }
 }
